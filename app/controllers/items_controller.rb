7611a49d8ed2f8df540abf869647f5a7df5a54f9
yoshimune1126 <redrice.is.god@gmail.com>
2024-08-01 20:00:13 +0900

    商品一覧表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,10 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-
+    @item = Item.all
+    @item = Item.order(created_at: :desc)
   end
+
 
   def new
     @item = Item.new
@@ -22,7 +24,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :item_description, :price, :image, :category_id, :condition_id, :prefecture_id, :shipping_duration_id, :shipping_fee_responsibility_id,) 
+    params.require(:item).permit(:item_name, :item_description, :price, :image, :category_id, :condition_id, :prefecture_id, :shipping_duration_id, :shipping_fee_responsibility_id,).merge(user_id: current_user.id)
   end
 
 
